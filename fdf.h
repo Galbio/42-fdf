5254be99055f29d0027840622fb25766e7021470
Gabriel Karbouch <gakarbou@c1r2p9.42perpignan.fr>
2024-12-19 22:57:37 +0100

    this nveer stops

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 16:30:07 by gakarbou          #+#    #+#             */
-/*   Updated: 2024/12/18 23:45:40 by gakarbou         ###   ########.fr       */
+/*   Updated: 2024/12/19 22:34:15 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ typedef struct s_cam_control
 	int	y_offset;
 	int	cam_zoom;
 	int	x_rotation;
+	int	y_rotation;
+	int	z_rotation;
 }	t_cam_control;
 
 typedef struct s_mlx_data
@@ -67,5 +69,6 @@ typedef struct s_line_points
 }	t_line_points;
 
 char	*get_next_line(int fd);
+char	draw_array(t_mlx_data *ds, int x, int y, t_line_points p);
 
 #endif
